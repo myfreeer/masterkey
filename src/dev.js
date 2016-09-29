840973b19cc42e6bd9932b3e37c7ede96cc63942
myfreeer <myfreeer@gmail.com>
2016-09-29 19:37:29 +0800

    Update dev.js

--- src/dev.js
+++ src/dev.js
@@ -1,55 +1,53 @@
-javascript:void((function() {
-    var masterkey = 'input your preferred masterKey here inside quotation marks';
-    var e = document.createElement('script');
-    e.setAttribute('src', 'https://myfreeer.github.io/masterkey/src/aes.js');
-    document.body.appendChild(e);
-    var F = document.createElement('script');
-    F.setAttribute('src', 'https://myfreeer.github.io/masterkey/src/sha256.js');
-    document.body.appendChild(F);
-    var G = document.createElement('script');
-    G.setAttribute('src', 'https://myfreeer.github.io/masterkey/src/base64.js');
-    document.body.appendChild(G);
-
-    function parseSha(sha, length) {
-        length = !!length ? length : sha.length / 2;
-        let bytesKey = new Array();
-        for (let i = 0; i < length; i++) {
-            bytesKey.push(parseInt(sha.substr(i * 2, 2), 16));
+//masterkey password generater by myfreeer
+//LGPL 3.0
+(function(root) {
+    "use strict";
+    const masterkey = {
+        parseSha: function(sha, length) {
+            let length1 = Number(length) || sha.length / 2;
+            let bytesKey = new Array();
+            for (let i = 0; i < length1; i++) {
+                bytesKey.push(parseInt(sha.substr(i * 2, 2), 16));
+            }
+            return new Uint8Array(bytesKey);
+        },
+        parseHostName: function(hostname) {
+            let hostNameArray = hostname.split('.');
+            let hostLength = hostNameArray.length;
+            let parsedHostName = hostLength > 1 ? hostNameArray[hostLength - 2] : hostname;
+            if (hostLength > 2)
+                if (hostNameArray[hostLength - 2].match(/com|org|net|int|edu|gov|mil/g)) parsedHostName = hostNameArray[hostLength - 3];
+            return parsedHostName;
+        },
+        makePassword: function(domain, masterKey, passwordLength1) {
+            //console.log(this);
+            let domainName = this.parseHostName(domain);
+            if (typeof masterKey == "undefined") {
+                console.log("masterKey is required to make a password");
+                return false;
+            }
+            let passwordLength = Number(passwordLength1) || 16;
+            let shaKey = sha512_256(masterKey);
+            let bytesKey = this.parseSha(shaKey);
+            let textBytes = aesjs.util.convertStringToBytes(domainName);
+            let aesCtr = new aesjs.ModeOfOperation.ctr(bytesKey);
+            let encryptedBytes = aesCtr.encrypt(textBytes);
+            let shaEncrypted = sha512(aesjs.util.convertBytesToString(encryptedBytes));
+            let password = shaEncrypted.substring(0, passwordLength);
+            return password;
+        },
+        //makePassword2 can have both uppercase and lowercase with a shortet length compared to makePassword
+        makePassword2: function(domain, masterKey, passwordLength1) {
+            let passwordLength = !!passwordLength1 ? passwordLength1 : 16;
+            let encodedPassword = Base64.encode(String.fromCharCode.apply(null, this.parseSha(this.makePassword(domain, masterKey, 128)))).replace(/[\\\/\+\=]/g, "");
+            return encodedPassword.substring(0, passwordLength);
         }
-        return new Uint8Array(bytesKey);
     }
-
-    function parseHostName(hostname) {
-        let hostNameArray = hostname.split('.');
-        let hostLength = hostNameArray.length;
-        let parsedHostName = hostLength > 1 ? hostNameArray[hostLength - 2] : hostname;
-        if (hostLength > 2)
-            if (hostNameArray[hostLength - 2].match(/com|org|net|int|edu|gov|mil/g)) parsedHostName = hostNameArray[hostLength - 3];
-        return parsedHostName;
+    if (typeof exports !== 'undefined') {
+        module.exports = masterkey;
+    } else if (typeof(define) === 'function' && define.amd) {
+        define(masterkey);
+    } else {
+        root.masterkey = masterkey;
     }
-
-    function makePassword(domain, masterKey, passwordLength1) {
-        let domainName = parseHostName(domain);
-        if (typeof masterKey == "undefined") {
-            console.log("masterKey is required to make a password");
-            return false;
-        }
-        let passwordLength = !!passwordLength1 ? passwordLength1 : 12;
-        let shaKey = sha256(masterKey);
-        let bytesKey = parseSha(shaKey);
-        let textBytes = aesjs.util.convertStringToBytes(domainName);
-        let aesCtr = new aesjs.ModeOfOperation.ctr(bytesKey);
-        let encryptedBytes = aesCtr.encrypt(textBytes);
-        let shaEncrypted = sha256(encryptedBytes)
-        let password = shaEncrypted.substring(0, passwordLength);
-        return password;
-    }
-
-    function makePassword2(domain, masterKey, passwordLength1) {
-        let passwordLength = !!passwordLength1 ? passwordLength1 : 12;
-        let encodedPassword = Base64.encode(String.fromCharCode.apply(null, parseSha(makePassword(domain, masterKey, passwordLength1)))).replace(/[\\\/\+\=]/g, "");
-        return encodedPassword.substring(0, passwordLength);
-    }
-
-    prompt('Password Generated for "' + parseHostName(location.hostname) + '" :', makePassword2(location.hostname, masterkey, 16));
-})())
+})(this);
